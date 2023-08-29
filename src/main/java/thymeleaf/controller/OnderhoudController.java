3ccbbaa20f8275653cb494dab296af8fcbdaec4f
huizecavia <bomaltennishal@outlook.com>
2023-08-29 11:04:35 +0200

    thymeleaf controller

--- src/main/java/thymeleaf/controller/OnderhoudController.java
+++ src/main/java/thymeleaf/controller/OnderhoudController.java
@@ -1,13 +1,15 @@
 package thymeleaf.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class OnderhoudController {
 
     @GetMapping
-    public String getIets() {
+    public String getIets(Model model) {
+        model.addAttribute("Kempten", "Straight Outta Controller");
         return "iets";
     }
 }
